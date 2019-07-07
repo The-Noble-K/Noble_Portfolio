243cf16d7dc011ae25ca63d058bc54442980c34a
The-Noble-K <jarod.sneed@gmail.com>
2019-07-07 00:39:46 -0400

    fixed url errors

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("projects/", include("projects.urls")),
-    path("blog/", include("blog.urls")),
+    path("", views.project_index, name="project_index"),
+    path("<int:pk>/", views.project_detail, name="project_detail"),
 ]
