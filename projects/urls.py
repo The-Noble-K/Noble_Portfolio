1a790beb635a26a18498a4d2e28db0aa82bfa0cd
The-Noble-K <jarod.sneed@gmail.com>
2019-07-07 00:28:01 -0400

    added blog

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path("", views.project_index, name="project_index"),
-    path("<int:pk>/", views.project_detail, name="project_detail"),
+    path("admin/", admin.site.urls),
+    path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 ]
